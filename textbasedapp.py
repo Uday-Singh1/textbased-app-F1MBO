6d6c9e1d9f1ed8c06f68ff3c5a5c8a3f0815d66a
Uday Singh <33316@ma-web.nl>
2021-11-01 16:39:27 +0100

    first commit

---
+++ textbasedapp.py
@@ -0,0 +1,230 @@
+vn = 0
+while vn == 0:
+    print("Hallo !,ik ben youssef")
+    c = input ("wie ben jij? ") 
+    print("wil je mijn verhaal horen?")
+    print("'ja'")
+    print("'nee'")
+    ant = input()
+    if ant == "ja":
+        print("oke dan,luister")
+        print("ik ben youssef karim en dit is mijn verhaal")
+        print("*flashback*")
+        vn = 1
+    elif ant == "nee":
+        print("oke dan niet !")
+        break
+while vn == 1:
+    print("je bent thuis en hoor knallen  van buiten maar in eerste instatie denk je dat het vuurwerk is maar uiteindelijk kom je er achter dat het bommen zijn die vallen")
+    print("ga je naar buiten of blijf je zitten ?")
+    print("'buiten'")
+    print("'blijf binnen'")
+    ant = input()
+    if ant == "buiten":
+     vn = 3
+    if ant == "blijf binnen":
+       vn = 2
+       print("je blijft zitten voor een tijdje totdat uiteindelijk uit een niets een soldaat je huis komt binnenstormen en alles steelt wat je hebt en vervolgens jouw vermoord omdat je de regering niet ondersteund")
+       break
+while vn == 3:  
+    print("je gaat naar buiten en ziet je buurman, vervolgens ga je naar je buurman om te vragen wat er is gebeurt en hij zegt")    
+    print("heb je de nieuws niet gezien we moeten nu weg er is oorlog in dit land")
+    print("we gaan nu weg ga je mee ?")
+    print("'mee gaan'")
+    print("'ga alleen'")
+    ant =input()
+    if ant == "mee gaan":
+      vn = 4
+    if ant == "ga alleen":
+      vn = 5
+while vn == 5:
+    print("je zegt ze gedag en wenst ze succes en je gaat weg")     
+    print("tijdens het lopen kom je een soldaat tegen die je aanvalt en knock out slaat")
+    print("hij neemt je mee en exucuteert je op bevel van zijn baas")
+    break
+while vn == 4:
+      
+      print("oke pak je spullen die je nog over hebt en kom gaan !")
+      
+      print("je pakt je spullen en gaat weg ")
+     
+      print("we gaan naar mijn broer in de volgende straat hij kent iemand die ons zou kunnen smokkelen naar verre landen ")
+      
+      print("je begint je reis met je buurman en zijn familie ")
+      
+      print("oke youssef we zijn nu inmiddels van damascus helemaal naar Alepo gegaan of ja we zijn bijna in Alepo")
+      
+      print("ga anders wat water drinker")
+     
+      print ("je drinkt water")
+      vn = 4 
+      
+      print("oke we zijn bij Alepo mijn broer zij dat hij aan de linker kant  van de grote moskee wachtte buiten de stad")
+      
+      print("je loopt en komt je buurman zijn broer tegen en loopt een stukje samen wat verder weg van de stad")
+      
+      print("jullie rusten allemaal en gaat wat eten en drinken")
+      
+      print("door de oorlog hadden jullie geen keus en moesten jullie eten en drinken stelen van de markt om te overleven")
+      
+      print("uiteindelijk vertrekken jullie naar naar salgin dat is de plek waar jullie de smokkelaar kunnen vinden")
+      
+      print("jullie vinden de smokkelaar en tellen wie er allemaal is 1,2,3,4,5 en gelukkig is iedereen er")  
+      
+      print("er waren ook famillies die helaas slachtoffers hadden")
+      
+      print("de smokkelaars maken de auto's klaar")
+      
+      print("ze veranderde de auto's op zo een manier dat er een luik in zit waar wij ons in konden verstoppen")
+      
+      print("ze hadden spullen op de luik gezet zodat het een heel moeilijk was om de luik te zien")
+      
+      print("voordat we vertrokken hadden we even snel gegeten, gedronken , geplast en gepoept")
+      
+      print("toen begon de reis naar turkije")
+      
+      print("we zouden dan naar istanbul gaan om vervolgens gesmokkeld te worden naar andere landen ")
+      
+      print("toen we uiteindelijk arriveerde bij de grens van Turkije werd er gecontroleerd")
+      
+      print("youssef hou je adem in en wees stil")
+      
+      print("'hou adem in'")
+      if ant == "hou adem in": 
+        vn = 4
+      ant = input()
+      
+      print("we moesten onze adem in houden en doodstil blijfen toen zij kwamen om te controleren")
+      
+      print("god zijn dank was alles goed gegaan en waren wij officeel in Turkije")
+      
+      print("we gingen tijdens de reis naar istanbul af en toe rusten en we voelden ons eindelijk een stuk veiliger")
+      
+      print("we vertelde elkaar ons verhaal en we troostte elkaar maar de verhalen die ik hoorde zijn verhalen waar ik nogsteeds van wakker lig")
+      
+      print("we waren uiteindelijk in istanboel en we konden onze keuzen maken om een land te kiezen waar we naar toe gingen")
+      
+      print("we kregen 4 keuzes ")
+
+      print("'Nederland ")
+      print("'switzerland'")
+      print("'Engeland'")
+      print("'duitsland'")
+
+      print("je buurman zei dat hij naar nederland ging en wou ook graag dat jij mee ging'")
+
+      print("waar ga jij naar toe ? zegt de smokkelaar")
+      
+      print("'Nederland ")
+      print("'switzerland'")
+      print("'Engeland'")
+      print("'duitsland'")
+      ant = input()
+      if ant =="Engeland":
+        vn =4
+      print("je wordt gesmokkeld naar Engeland en dan leef je daar je leven")
+      
+      print("je vind daar een vrouw die ook van syrisch afkomst is en trouwt met haar na 2 jaar na ontmoeting")
+      
+      print("je krijgt drie kinderen 2 jongens en 1 meisje na een lange tijd hebben alle 3 kinderen een goeie studie")
+      
+      print("wanneer je 80 jaar oud word krijg je raare en vreemden gedachtes en krijg je trauma's van je verleden en die komen dan ook terug in je dromen")  
+      
+      print("•flashback voorbij•")
+      
+      print("dat was mijn verhaal ik hoop dat je er wat van hebt geleerd zegt youssef")
+      
+      print("dat heb ik zeker bedankt voor uw verhaal")
+      
+      print("je gaat weg")
+      
+      print("youssef zegt 'ik denk wel dat ik een grote verschil heb gemaakt in het leven en voor mijn familie")
+      
+      print("god ik dank u elke dag en ook voor de mogelijkheid om de juiste keuzes te hebben gemaakt in mijn hele leven")
+      
+      print("youssef doet zijn oge dicht en sterft dan in vreden op een 90 jarige leeftijd .")  
+     
+      ant = input()
+      if ant =="switserland":
+         vn =4 
+      print("je wordt gesmokkeld naar switserland en dan leef je daar je leven")
+      
+      print("je vind daar een vrouw die ook van syrisch afkomst is en trouwt met haar na 2 jaar na ontmoeting")
+      
+      print("je krijgt vier kinderen 1 jongen en 3 meisje na een lange tijd hebben alle 4 kinderen een goeie studie")
+      
+      print("wanneer je 76 jaar oud word krijg je raare en vreemden gedachtes en krijg je trauma's van je verleden en die komen dan ook terug in je dromen")  
+      
+      print("•flashback voorbij•")
+      
+      print("dat was mijn verhaal ik hoop dat je er wat van hebt geleerd zegt youssef")
+      
+      print("dat heb ik zeker bedankt voor uw verhaal")
+      
+      print("je gaat weg")
+      
+      print("youssef zegt 'ik denk wel dat ik een grote verschil heb gemaakt in het leven en voor mijn familie")
+      
+      print("god ik dank u elke dag en ook voor de mogelijkheid om de juiste keuzes te hebben gemaakt in mijn hele leven")
+      
+      print("youssef doet zijn oge dicht en sterft dan in vreden op een 92 jarige leeftijd .")  
+       
+      ant = input()
+      if ant =="duitsland":
+           vn=4
+
+           print("je wordt gesmokkeld naar switserland en dan leef je daar je leven")
+      
+      print("je vind daar een vrouw die ook van syrisch afkomst is en trouwt met haar na 2 jaar na ontmoeting")
+      
+      print("je krijgt 6 kinderen 4 jongen en 2 meisje na een lange tijd hebben alle 6 kinderen een goeie studie")
+
+      print("de oudste jongen open zelfs zijn eigen beroemde bedrijf dat tech producten maakt en verkoopt")
+      
+      print("wanneer je 83 jaar oud word krijg je raare en vreemden gedachtes en krijg je trauma's van je verleden en die komen dan ook terug in je dromen")  
+      
+      print("•flashback voorbij•")
+      
+      print("dat was mijn verhaal ik hoop dat je er wat van hebt geleerd zegt youssef")
+      
+      print("dat heb ik zeker bedankt voor uw verhaal")
+      
+      print("je gaat weg")
+      
+      print("youssef zegt 'ik denk wel dat ik een grote verschil heb gemaakt in het leven en voor mijn familie")
+      
+      print("god ik dank u elke dag en ook voor de mogelijkheid om de juiste keuzes te hebben gemaakt in mijn hele leven")
+      
+      print("youssef doet zijn oge dicht en sterft dan in vreden op een 102 jarige leeftijd .")
+
+      ant = input()
+      if ant == "nederland":
+        vn=4
+
+        print("je wordt gesmokkeld naar nederland en dan leef je daar je leven")
+      
+      print("je vind daar een vrouw die  van nederlands afkomst is en trouwt met haar na 1,5 jaar na ontmoeting")
+      
+      print("je krijgt 2 kinderen 1 jongen en 1 meisje na een lange tijd hebben de  2 kinderen een goeie studie")
+      
+      print("wanneer je 89 jaar oud word krijg je raare en vreemden gedachtes en krijg je trauma's van je verleden en die komen dan ook terug in je dromen")  
+      print("maar wanneer je jouw kinderen ziet zijn die trauma's al heel snel weg")
+      print("•flashback voorbij•")
+      
+      print("dat was mijn verhaal ik hoop dat je er wat van hebt geleerd zegt youssef")
+      
+      print("dat heb ik zeker bedankt voor uw verhaal")
+      
+      print("je gaat weg")
+      
+      print("youssef zegt 'ik denk wel dat ik een grote verschil heb gemaakt in het leven en voor mijn familie")
+      
+      print("god ik dank u elke dag en ook voor de mogelijkheid om de juiste keuzes te hebben gemaakt in mijn hele leven")
+      print("youssef doet zijn oge dicht en sterft dan in vreden op een 105 jarige leeftijd .")
+      
+
+
+
+
+ 
+
